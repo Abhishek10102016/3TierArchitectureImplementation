ff9fceb9c782da77caf5565b70f9908942fc2178
Abhishek10102016 <abhisheklovesthakur@gmail.com>
2023-06-02 15:35:48 +0530

    Add project files.

---
+++ BusinessLayer/ClassBLL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace BusinessLayer
+{
+    public class ClassBLL
+    {
+        public Learn_DBEntities _context;
+        public ClassBLL()
+        {
+            _context = new Learn_DBEntities();
+        }
+        public List<Employee> namesDetail()
+        {
+            return _context.Employees.ToList();
+        }
+    }
+}
---
+++ PresentationLayer/Controllers/EmployeeDetailsController.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer;
+using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    public class EmployeeDetailsController : Controller
+    {
+        // GET: EmployeeDetails
+        public ActionResult Index()
+        {
+            var employee = new ClassBLL();
+            List<Employee> employeeName = employee.namesDetail();
+            return View(employeeName);
+        }
+    }
+}
